22a4adc96cb616e903bdf06983c1a650f95fcced
Nikolaev Viktor <maclauddd@gmail.com>
2020-06-13 19:56:45 +0300

    lifting state up

--- src/tic-tac-toe/src/index.js
+++ src/tic-tac-toe/src/index.js
@@ -3,30 +3,40 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: props.value,
-    };
-  }
-
-  onClickHandler = () => {
-    this.setState({ value: "X" });
-  };
-
   render() {
+    const { value, onClick } = this.props;
+
     return (
-      <button className="square" onClick={this.onClickHandler}>
-        {this.state.value}
+      <button className="square" onClick={onClick}>
+        {value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick = (i) => {
+    const squares = this.state.squares.slice();
+
+    squares[i] = "X";
+    this.setState({ squares });
+  };
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
